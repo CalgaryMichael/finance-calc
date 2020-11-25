10fd57421db1ce874f0410e672cc5e284d905de8
Calgary Michael <cseth.michael@gmail.com>
2020-11-24 22:55:52 -0500

    Docker!

--- finance-calc/api/server.go
+++ finance-calc/api/server.go
@@ -2,15 +2,24 @@ package api
 
 import (
 	"log"
-	http "net/http"
+	"net/http"
 
 	mux "github.com/gorilla/mux"
 )
 
-func StartServer() {
-	r := mux.NewRouter()
-	r.HandleFunc("/project", projectScenario).Methods("POST")
+type MuxRouteFunc func(http.ResponseWriter, *http.Request)
 
-	log.Fatal(http.ListenAndServe("127.0.0.1:3000", r))
-	log.Print("Now listening on port 3000...")
+func requestLogger(f MuxRouteFunc) MuxRouteFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%v] %v\n", r.Method, r.URL)
+		f(w, r)
+	}
+}
+
+func StartServer() {
+	log.Println("Starting server on :3000...")
+	r := mux.NewRouter()
+	r.HandleFunc("/project", requestLogger(projectScenario)).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
